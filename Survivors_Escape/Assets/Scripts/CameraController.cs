b38e29970d9ecd21512fba8e960a02f0c50bf99d
Mauricio Landeros <jairo.landeros2885@alumnos.udg.mx>
2023-06-19 17:26:42 -0600

    Camera control

---
+++ Survivors_Escape/Assets/Scripts/CameraController.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Framing")]
+    [SerializeField] private Camera _camera = null;
+    [SerializeField] private Transform _followTransform= null;
+    [SerializeField] private Vector3 _framing=new Vector3(0, 0,0);
+
+    [Header("Distance")]
+    [SerializeField] private float _zoomSpeed = 10f;
+    [SerializeField] private float _defaultDistance = 5f;
+    [SerializeField] private float _minDistance = 0f;
+    [SerializeField] private float _maxDistance = 10f;
+
+    [Header("Rotation")]
+    [SerializeField] private bool _invertX = false;
+    [SerializeField] private bool _invertY = false;
+    [SerializeField] private float _mouseSensitivity = 1f;
+    [SerializeField] private float _rotationSharpness = 25f;
+    [SerializeField] private float _defaultVerticalAngle = 20f;
+    [SerializeField][Range(-90, 90)] private float _minVerticalAngle = -90.0f;
+    [SerializeField][Range(-90, 90)] private float _maxVerticalAngle = 90.0f;
+
+    [Header("Obstruction")]
+    [SerializeField] private float _checkRadius = 0.2f;
+    [SerializeField] private LayerMask _obstructionLayers = -1;
+    private List<Collider> _ignoreColliders = new List<Collider>();
+
+    private Vector3 _planarDirection;
+    private Quaternion _targetRotation;
+    private float _targetVerticalAngle;
+    private float _targetDistance;
+    private Vector3 _targetPosition;
+
+    private Vector3 _newPosition;
+    private Quaternion _newRotation;
+
+    public Vector3 _cameraPlanarDirection { get => _planarDirection; }
+
+    private void OnValidate()
+    {
+        _defaultDistance = Mathf.Clamp(_defaultDistance, _minDistance, _maxDistance);
+        _defaultVerticalAngle = Mathf.Clamp(_defaultVerticalAngle, _minVerticalAngle, _maxVerticalAngle);
+    }
+
+    private void Start()
+    {
+        _ignoreColliders.AddRange(GetComponentsInChildren<Collider>());
+        _planarDirection = _followTransform.forward;
+        _targetDistance = _defaultDistance;
+        _targetVerticalAngle = _defaultVerticalAngle;
+
+        _targetRotation = Quaternion.LookRotation(_planarDirection) * Quaternion.Euler(_targetVerticalAngle, 0, 0);
+        _targetPosition = _followTransform.position - (_targetRotation * Vector3.forward) * _targetDistance;
+
+        Cursor.lockState=CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (Cursor.lockState != CursorLockMode.Locked)
+            return;
+
+        #region MOUSE INPUT HANDLING
+        float mouseX = InputManager.MouseXInput * _mouseSensitivity;
+        float mouseY = -InputManager.MouseYInput * _mouseSensitivity;
+        float zoom = -InputManager.MouseScrollInput * _zoomSpeed;
+
+        if (_invertX) { mouseX *= -1f; }
+        if (_invertY) { mouseX *= -1f; }
+
+        Vector3 focusPosition = _followTransform.position + _camera.transform.TransformDirection(_framing);
+        _planarDirection = Quaternion.Euler(0, mouseX, 0) * _planarDirection;
+        _targetVerticalAngle = Mathf.Clamp(_targetVerticalAngle + mouseY, _minVerticalAngle, _maxVerticalAngle);
+        _targetDistance = Mathf.Clamp(_targetDistance + zoom, _minDistance, _maxDistance);
+
+        float smallestDistance = _targetDistance;
+        RaycastHit[] hits = Physics.SphereCastAll(
+                focusPosition,
+                _checkRadius,
+                _targetRotation * -Vector3.forward,
+                _targetDistance,
+                _obstructionLayers);
+
+        if (hits.Length > 0)
+        {
+            foreach (RaycastHit hit in hits)
+            {
+                if (!_ignoreColliders.Contains(hit.collider))
+                {
+                    if (hit.distance < smallestDistance)
+                    {
+                        smallestDistance = hit.distance;
+                    }
+                }
+            }
+        }
+
+        _targetRotation = Quaternion.LookRotation(_planarDirection) * Quaternion.Euler(_targetVerticalAngle, 0, 0);
+        _targetPosition = focusPosition - (_targetRotation * Vector3.forward) * smallestDistance;
+
+        _newRotation = Quaternion.Slerp(_camera.transform.rotation, _targetRotation, Time.deltaTime * _rotationSharpness);
+        _newPosition = Vector3.Lerp(_camera.transform.position, _targetPosition, Time.deltaTime * _rotationSharpness);
+
+        _camera.transform.rotation = _newRotation;
+        _camera.transform.position = _newPosition;
+        #endregion
+    }
+}
---
+++ Survivors_Escape/Assets/Scripts/InputManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[System.Serializable]
+public class Input
+{
+    public KeyCode _primary;
+    public KeyCode _alternate;
+
+    public bool Pressed()
+    {
+        return UnityEngine.Input.GetKey(_primary) || UnityEngine.Input.GetKey(_alternate);
+    }
+
+    public bool PressedDown()
+    {
+        return UnityEngine.Input.GetKeyDown(_primary) || UnityEngine.Input.GetKeyDown(_alternate);
+    }
+
+    public bool PressedUp()
+    {
+        return UnityEngine.Input.GetKeyUp(_primary) || UnityEngine.Input.GetKeyUp(_alternate);
+    }
+}
+
+public class InputManager : MonoBehaviour
+{
+    public Input Forward;
+    public Input Backward;
+    public Input Right;
+    public Input Left;
+    public Input Sprint;
+
+    public int MoveAxisForward
+    {
+        get
+        {
+            if (Forward.Pressed() && Backward.Pressed()) { return 0; }
+            else if (Forward.Pressed()) { return 1; }
+            else if (Backward.Pressed()) { return -1; }
+            else { return 0; }
+        }
+    }
+
+    public int MoveAxisRight
+    {
+        get
+        {
+            if (Right.Pressed() && Left.Pressed()) { return 0; }
+            else if (Right.Pressed()) { return 1; }
+            else if (Left.Pressed()) { return -1; }
+            else { return 0; }
+        }
+    }
+
+    #region MOUSE PARAMS
+    public const string MouseX = "Mouse X";
+    public const string MouseY = "Mouse Y";
+    public const string MouseScroll = "Mouse ScrollWheel";
+
+    public static float MouseXInput { get => UnityEngine.Input.GetAxis(MouseX); }
+    public static float MouseYInput { get => UnityEngine.Input.GetAxis(MouseY); }
+    public static float MouseScrollInput { get => UnityEngine.Input.GetAxis(MouseScroll); }
+    #endregion
+}
