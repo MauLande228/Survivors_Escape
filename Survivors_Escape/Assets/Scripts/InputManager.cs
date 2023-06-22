139f0f28dc9b262d0e8d0600e6a0d452f4b268e0
Mauricio Landeros <jairo.landeros2885@alumnos.udg.mx>
2023-06-21 22:36:16 -0600

    Character stances and animation transitions

--- Survivors_Escape/Assets/Scripts/InputManager.cs
+++ Survivors_Escape/Assets/Scripts/InputManager.cs
@@ -3,65 +3,70 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-[System.Serializable]
-public class Input
+namespace SurvivorsEscape
 {
-    public KeyCode _primary;
-    public KeyCode _alternate;
-
-    public bool Pressed()
+    [System.Serializable]
+    public class Input
     {
-        return UnityEngine.Input.GetKey(_primary) || UnityEngine.Input.GetKey(_alternate);
-    }
+        public KeyCode _primary;
+        public KeyCode _alternate;
 
-    public bool PressedDown()
-    {
-        return UnityEngine.Input.GetKeyDown(_primary) || UnityEngine.Input.GetKeyDown(_alternate);
-    }
-
-    public bool PressedUp()
-    {
-        return UnityEngine.Input.GetKeyUp(_primary) || UnityEngine.Input.GetKeyUp(_alternate);
-    }
-}
-
-public class InputManager : MonoBehaviour
-{
-    public Input Forward;
-    public Input Backward;
-    public Input Right;
-    public Input Left;
-    public Input Sprint;
-
-    public int MoveAxisForward
-    {
-        get
+        public bool Pressed()
         {
-            if (Forward.Pressed() && Backward.Pressed()) { return 0; }
-            else if (Forward.Pressed()) { return 1; }
-            else if (Backward.Pressed()) { return -1; }
-            else { return 0; }
+            return UnityEngine.Input.GetKey(_primary) || UnityEngine.Input.GetKey(_alternate);
+        }
+
+        public bool PressedDown()
+        {
+            return UnityEngine.Input.GetKeyDown(_primary) || UnityEngine.Input.GetKeyDown(_alternate);
+        }
+
+        public bool PressedUp()
+        {
+            return UnityEngine.Input.GetKeyUp(_primary) || UnityEngine.Input.GetKeyUp(_alternate);
         }
     }
 
-    public int MoveAxisRight
+    public class InputManager : MonoBehaviour
     {
-        get
+        public Input Forward;
+        public Input Backward;
+        public Input Right;
+        public Input Left;
+        public Input Sprint;
+        public Input Crouch;
+        public Input Prone;
+
+        public int MoveAxisForward
         {
-            if (Right.Pressed() && Left.Pressed()) { return 0; }
-            else if (Right.Pressed()) { return 1; }
-            else if (Left.Pressed()) { return -1; }
-            else { return 0; }
+            get
+            {
+                if (Forward.Pressed() && Backward.Pressed()) { return 0; }
+                else if (Forward.Pressed()) { return 1; }
+                else if (Backward.Pressed()) { return -1; }
+                else { return 0; }
+            }
         }
+
+        public int MoveAxisRight
+        {
+            get
+            {
+                if (Right.Pressed() && Left.Pressed()) { return 0; }
+                else if (Right.Pressed()) { return 1; }
+                else if (Left.Pressed()) { return -1; }
+                else { return 0; }
+            }
+        }
+
+        #region MOUSE PARAMS
+        public const string MouseX = "Mouse X";
+        public const string MouseY = "Mouse Y";
+        public const string MouseScroll = "Mouse ScrollWheel";
+
+        public static float MouseXInput { get => UnityEngine.Input.GetAxis(MouseX); }
+        public static float MouseYInput { get => UnityEngine.Input.GetAxis(MouseY); }
+        public static float MouseScrollInput { get => UnityEngine.Input.GetAxis(MouseScroll); }
+        #endregion
     }
-
-    #region MOUSE PARAMS
-    public const string MouseX = "Mouse X";
-    public const string MouseY = "Mouse Y";
-    public const string MouseScroll = "Mouse ScrollWheel";
-
-    public static float MouseXInput { get => UnityEngine.Input.GetAxis(MouseX); }
-    public static float MouseYInput { get => UnityEngine.Input.GetAxis(MouseY); }
-    public static float MouseScrollInput { get => UnityEngine.Input.GetAxis(MouseScroll); }
-    #endregion
 }
